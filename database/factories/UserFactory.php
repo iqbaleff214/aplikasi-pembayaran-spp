fb7fb65710c9b01bc712073237d3fbb6f6514272
M. Iqbal Effendi <iqbaleff214@gmail.com>
2023-02-19 20:17:18 +0800

    remove unnecessary column for user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,6 @@ class UserFactory extends Factory
         return [
             'name' => fake('id_ID')->name(),
             'username' => $username,
-            'email_verified_at' => now(),
             'password' => Hash::make($username),
             'role' => Role::STAFF,
         ];
